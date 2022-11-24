4ae16d7eda1da8a7c50289e099a5a9ebc3807c3e
Temimeno <kittipitch.mold@mail.kmutt.ac.th>
2022-11-24 22:24:05 +0700

    Homework 3 : Num1 and Num2

---
+++ Num1/mdt211_2565_assignment3-main/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<char> Steering_Wheel = new Queue<char>();
+
+        int Stopper = 1;
+
+        do {
+            char Input = char.Parse(Console.ReadLine());
+
+            switch(Input) {
+                case 'J':
+                case 'j': {
+                    Steering_Wheel.Push('J');
+                    break;
+                }
+                case 'G': 
+                case 'g': {
+                    Steering_Wheel.Push('G');
+                    break;
+                }
+                case 'O':
+                case 'o': {
+                    Steering_Wheel.Push('O');
+                    break;
+                }
+                case 'R':
+                case 'r': {
+                    Steering_Wheel.Push('R');
+                    break;
+                }
+                default: {
+                    Stopper = 0;
+                    break;
+                }
+            }
+        } while(Stopper == 1);
+
+        for(int i = 0; i < Steering_Wheel.GetLength(); i++) {
+            Console.Write("{0}", Steering_Wheel.Get(i));
+        }
+    }
+}
---
+++ Num2/mdt211_2565_assignment3-main/Program.cs
@@ -0,0 +1,60 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> FruitInput = new Queue<int>();
+        Queue<int> FirstCut = new Queue<int>();
+        Queue<int> SecondCut = new Queue<int>();
+
+        int Stopper = 1;
+
+        do {
+            char FruitSize = char.Parse(Console.ReadLine());
+
+            switch(FruitSize) {
+                case 'S':
+                case 's': {
+                    FruitInput.Push(3);
+                    break;
+                }
+                case 'M': 
+                case 'm': {
+                    FruitInput.Push(2);
+
+                    for(int i = 1; i <= 3; i++) {
+                        FirstCut.Push(3);
+                    }
+                    break;
+                }
+                case 'L':
+                case 'l': {
+                    FruitInput.Push(1);
+
+                    for(int i =1; i <=2; i++) {
+                        FirstCut.Push(2);
+
+                        for(int j = 1; j <= 3; j++) {
+                            SecondCut.Push(3);
+                        }
+                    }
+                    break;
+                }
+                default: {
+                    Stopper = 0;
+                    break;
+                }
+            }
+        } while(Stopper == 1);
+
+        for(int i = 0; i < FruitInput.GetLength(); i++) {
+            Console.Write("{0}", FruitInput.Get(i));
+        }
+        for(int i = 0; i < FirstCut.GetLength(); i++) {
+            Console.Write("{0}", FirstCut.Get(i));
+        }
+        for(int i = 0; i < SecondCut.GetLength(); i++) {
+            Console.Write("{0}", SecondCut.Get(i));
+        }
+
+    }
+}
